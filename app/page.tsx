0d380dcc8405c8486ad3a3bd40199c8df5e233b5
JeanCornelio <jean_cornelio17@hotmail.com>
2026-03-30 21:49:37 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,36 @@
-import Image from "next/image";
+import { GithubSvg } from "@/components/common/svg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="relative h-dvh">
+      <main className=" gap-10 max-w-6xl mx-auto  items-center justify-center">
+        <div className="flex flex-col gap-6 mt-40">
+          <h1 className="text-3xl sm:text-5xl lg:text-8xl font-bold text-center dark:text-white ">
+            From <span className="text-primary">GitHub</span> to <br></br>
+            <span className="text-primary">portfolio</span> in one click.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="sm:text-lg xl:text-2xl text-gray-600 dark:text-gray-400 text-center">
+            Connect your GitHub and we&apos;ll build it for you. <br></br> Ready
+            to share in under a minute.{" "}
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <div className="flex gap-3 justify-center">
+            <Button className=" p-5" size={"lg"}>
+              <GithubSvg />
+              Get started
+            </Button>
+
+            <Link href={"/how-it-works"}>
+              {" "}
+              <Button className="p-5 " variant={"outline"} size={"lg"}>
+                How it works?
+              </Button>
+            </Link>
+          </div>
         </div>
       </main>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
     </div>
   );
 }
---
+++ components/common/svg.tsx
@@ -0,0 +1,199 @@
+import { SVGProps } from "react";
+
+export const GithubSvg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill={props.fill || "#fff"}
+    viewBox="0 -0.5 25 25"
+    {...props}>
+    <path d="M12.301 0h.093c2.242 0 4.34.613 6.137 1.68l-.055-.031a12.351 12.351 0 0 1 4.449 4.422l.031.058a12.182 12.182 0 0 1 1.654 6.166c0 5.406-3.483 10-8.327 11.658l-.087.026a.724.724 0 0 1-.642-.113l.002.001a.624.624 0 0 1-.208-.466v-.014.001l.008-1.226q.008-1.178.008-2.154a2.844 2.844 0 0 0-.833-2.274 10.918 10.918 0 0 0 1.718-.305l-.076.017a6.508 6.508 0 0 0 1.537-.642l-.031.017a4.52 4.52 0 0 0 1.292-1.058l.006-.007a4.9 4.9 0 0 0 .84-1.645l.009-.035a7.888 7.888 0 0 0 .329-2.281l-.001-.136v.007l.001-.072a4.73 4.73 0 0 0-1.269-3.23l.003.003c.168-.44.265-.948.265-1.479a4.25 4.25 0 0 0-.404-1.814l.011.026a2.095 2.095 0 0 0-1.31.181l.012-.005a8.622 8.622 0 0 0-1.512.726l.038-.022-.609.384c-.922-.264-1.981-.416-3.075-.416s-2.153.152-3.157.436l.081-.02q-.256-.176-.681-.433a9.103 9.103 0 0 0-1.272-.595l-.066-.022A2.174 2.174 0 0 0 5.837 5.1l.013-.002a4.2 4.2 0 0 0-.393 1.788c0 .531.097 1.04.275 1.509l-.01-.029a4.723 4.723 0 0 0-1.265 3.303v-.004l-.001.13c0 .809.12 1.591.344 2.327l-.015-.057c.189.643.476 1.202.85 1.693l-.009-.013a4.35 4.35 0 0 0 1.267 1.062l.022.011c.432.252.933.465 1.46.614l.046.011c.466.125 1.024.227 1.595.284l.046.004c-.431.428-.718 1-.784 1.638l-.001.012a3.056 3.056 0 0 1-.699.236l-.021.004c-.256.051-.549.08-.85.08h-.066.003a1.882 1.882 0 0 1-1.055-.348l.006.004a2.84 2.84 0 0 1-.881-.986l-.007-.015a2.603 2.603 0 0 0-.768-.827l-.009-.006a2.331 2.331 0 0 0-.776-.38l-.016-.004-.32-.048a1.048 1.048 0 0 0-.471.074l.007-.003q-.128.072-.08.184c.039.086.087.16.145.225l-.001-.001c.061.072.13.135.205.19l.003.002.112.08c.283.148.516.354.693.603l.004.006c.191.237.359.505.494.792l.01.024.16.368c.135.402.38.738.7.981l.005.004c.3.234.662.402 1.057.478l.016.002c.33.064.714.104 1.106.112h.007c.045.002.097.002.15.002.261 0 .517-.021.767-.062l-.027.004.368-.064q0 .609.008 1.418t.008.873v.014c0 .185-.08.351-.208.466h-.001a.717.717 0 0 1-.645.111l.005.001C3.486 22.286.006 17.692.006 12.285c0-2.268.612-4.393 1.681-6.219l-.032.058a12.351 12.351 0 0 1 4.422-4.449l.058-.031a11.898 11.898 0 0 1 6.073-1.645h.098-.005zm-7.64 17.666q.048-.112-.112-.192-.16-.048-.208.032-.048.112.112.192.144.096.208-.032zm.497.545q.112-.08-.032-.256-.16-.144-.256-.048-.112.08.032.256.159.157.256.047zm.48.72q.144-.112 0-.304-.128-.208-.272-.096-.144.08 0 .288t.272.112zm.672.673q.128-.128-.064-.304-.192-.192-.32-.048-.144.128.064.304.192.192.32.044zm.913.4q.048-.176-.208-.256-.24-.064-.304.112t.208.24q.24.097.304-.096zm1.009.08q0-.208-.272-.176-.256 0-.256.176 0 .208.272.176.256.001.256-.175zm.929-.16q-.032-.176-.288-.144-.256.048-.224.24t.288.128.225-.224z" />
+  </svg>
+);
+
+export const PortifySvg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="283.5 236.15 243 307.55"
+    {...props}>
+    <defs>
+      <clipPath id="a">
+        <path d="M283.5 236.152h243v308.496h-243Zm0 0" />
+      </clipPath>
+      <clipPath id="b">
+        <path d="M327 236.152h156a43.501 43.501 0 0 1 43.5 43.5v220.942a43.489 43.489 0 0 1-12.742 30.758A43.489 43.489 0 0 1 483 544.094H327a43.489 43.489 0 0 1-30.758-12.742 43.489 43.489 0 0 1-12.742-30.758V279.652a43.498 43.498 0 0 1 43.5-43.5Zm0 0" />
+      </clipPath>
+      <clipPath id="d">
+        <path d="M.5.152h243V307.7H.5Zm0 0" />
+      </clipPath>
+      <clipPath id="e">
+        <path d="M44 .152h156a43.501 43.501 0 0 1 43.5 43.5v220.942a43.489 43.489 0 0 1-12.742 30.758A43.489 43.489 0 0 1 200 308.094H44a43.489 43.489 0 0 1-30.758-12.742A43.489 43.489 0 0 1 .5 264.594V43.652A43.498 43.498 0 0 1 44 .152Zm0 0" />
+      </clipPath>
+      <clipPath id="c">
+        <path d="M0 0h244v309H0z" />
+      </clipPath>
+      <clipPath id="g">
+        <path d="M0 0h195v390H0z" />
+      </clipPath>
+      <linearGradient
+        id="f"
+        x1={0}
+        x2={256}
+        y1={162.208}
+        y2={162.208}
+        gradientTransform="matrix(.94922 0 0 .94922 .5 .147)"
+        gradientUnits="userSpaceOnUse">
+        <stop offset={0} stopColor="#FF3E9B" />
+        <stop offset={0.008} stopColor="#FF3E9B" />
+        <stop offset={0.016} stopColor="#FE3E9A" />
+        <stop offset={0.023} stopColor="#FD3E9A" />
+        <stop offset={0.031} stopColor="#FC3E9A" />
+        <stop offset={0.039} stopColor="#FB3E99" />
+        <stop offset={0.047} stopColor="#FA3E99" />
+        <stop offset={0.055} stopColor="#FA3E98" />
+        <stop offset={0.063} stopColor="#F93E98" />
+        <stop offset={0.07} stopColor="#F83E97" />
+        <stop offset={0.078} stopColor="#F73E97" />
+        <stop offset={0.086} stopColor="#F63D97" />
+        <stop offset={0.094} stopColor="#F53D96" />
+        <stop offset={0.102} stopColor="#F53D96" />
+        <stop offset={0.109} stopColor="#F43D95" />
+        <stop offset={0.117} stopColor="#F33D95" />
+        <stop offset={0.125} stopColor="#F23D94" />
+        <stop offset={0.133} stopColor="#F13D94" />
+        <stop offset={0.141} stopColor="#F03D94" />
+        <stop offset={0.148} stopColor="#F03D93" />
+        <stop offset={0.156} stopColor="#EF3D93" />
+        <stop offset={0.164} stopColor="#EE3D92" />
+        <stop offset={0.172} stopColor="#ED3D92" />
+        <stop offset={0.18} stopColor="#EC3D91" />
+        <stop offset={0.188} stopColor="#EB3D91" />
+        <stop offset={0.195} stopColor="#EB3D91" />
+        <stop offset={0.203} stopColor="#EA3D90" />
+        <stop offset={0.211} stopColor="#E93D90" />
+        <stop offset={0.219} stopColor="#E83D8F" />
+        <stop offset={0.227} stopColor="#E73D8F" />
+        <stop offset={0.234} stopColor="#E63D8F" />
+        <stop offset={0.242} stopColor="#E63D8E" />
+        <stop offset={0.25} stopColor="#E53D8E" />
+        <stop offset={0.258} stopColor="#E43C8D" />
+        <stop offset={0.266} stopColor="#E33C8D" />
+        <stop offset={0.273} stopColor="#E23C8C" />
+        <stop offset={0.281} stopColor="#E13C8C" />
+        <stop offset={0.289} stopColor="#E03C8C" />
+        <stop offset={0.297} stopColor="#E03C8B" />
+        <stop offset={0.305} stopColor="#DF3C8B" />
+        <stop offset={0.313} stopColor="#DE3C8A" />
+        <stop offset={0.32} stopColor="#DD3C8A" />
+        <stop offset={0.328} stopColor="#DC3C89" />
+        <stop offset={0.336} stopColor="#DB3C89" />
+        <stop offset={0.344} stopColor="#DB3C89" />
+        <stop offset={0.352} stopColor="#DA3C88" />
+        <stop offset={0.359} stopColor="#D93C88" />
+        <stop offset={0.367} stopColor="#D83C87" />
+        <stop offset={0.375} stopColor="#D73C87" />
+        <stop offset={0.383} stopColor="#D63C87" />
+        <stop offset={0.391} stopColor="#D63C86" />
+        <stop offset={0.398} stopColor="#D53C86" />
+        <stop offset={0.406} stopColor="#D43C85" />
+        <stop offset={0.414} stopColor="#D33C85" />
+        <stop offset={0.422} stopColor="#D23B84" />
+        <stop offset={0.43} stopColor="#D13B84" />
+        <stop offset={0.438} stopColor="#D13B84" />
+        <stop offset={0.445} stopColor="#D03B83" />
+        <stop offset={0.453} stopColor="#CF3B83" />
+        <stop offset={0.461} stopColor="#CE3B82" />
+        <stop offset={0.469} stopColor="#CD3B82" />
+        <stop offset={0.477} stopColor="#CC3B81" />
+        <stop offset={0.484} stopColor="#CC3B81" />
+        <stop offset={0.492} stopColor="#CB3B81" />
+        <stop offset={0.5} stopColor="#CA3B80" />
+        <stop offset={0.508} stopColor="#C93B80" />
+        <stop offset={0.516} stopColor="#C83B7F" />
+        <stop offset={0.523} stopColor="#C73B7F" />
+        <stop offset={0.531} stopColor="#C73B7F" />
+        <stop offset={0.539} stopColor="#C63B7E" />
+        <stop offset={0.547} stopColor="#C53B7E" />
+        <stop offset={0.555} stopColor="#C43B7D" />
+        <stop offset={0.563} stopColor="#C33B7D" />
+        <stop offset={0.57} stopColor="#C23B7C" />
+        <stop offset={0.578} stopColor="#C23B7C" />
+        <stop offset={0.586} stopColor="#C13B7C" />
+        <stop offset={0.594} stopColor="#C03A7B" />
+        <stop offset={0.602} stopColor="#BF3A7B" />
+        <stop offset={0.609} stopColor="#BE3A7A" />
+        <stop offset={0.617} stopColor="#BD3A7A" />
+        <stop offset={0.625} stopColor="#BD3A79" />
+        <stop offset={0.633} stopColor="#BC3A79" />
+        <stop offset={0.641} stopColor="#BB3A79" />
+        <stop offset={0.648} stopColor="#BA3A78" />
+        <stop offset={0.656} stopColor="#B93A78" />
+        <stop offset={0.664} stopColor="#B83A77" />
+        <stop offset={0.672} stopColor="#B83A77" />
+        <stop offset={0.68} stopColor="#B73A77" />
+        <stop offset={0.688} stopColor="#B63A76" />
+        <stop offset={0.695} stopColor="#B53A76" />
+        <stop offset={0.703} stopColor="#B43A75" />
+        <stop offset={0.711} stopColor="#B33A75" />
+        <stop offset={0.719} stopColor="#B33A74" />
+        <stop offset={0.727} stopColor="#B23A74" />
+        <stop offset={0.734} stopColor="#B13A74" />
+        <stop offset={0.742} stopColor="#B03A73" />
+        <stop offset={0.75} stopColor="#AF3A73" />
+        <stop offset={0.758} stopColor="#AE3972" />
+        <stop offset={0.766} stopColor="#AD3972" />
+        <stop offset={0.773} stopColor="#AD3971" />
+        <stop offset={0.781} stopColor="#AC3971" />
+        <stop offset={0.789} stopColor="#AB3971" />
+        <stop offset={0.797} stopColor="#AA3970" />
+        <stop offset={0.805} stopColor="#A93970" />
+        <stop offset={0.813} stopColor="#A8396F" />
+        <stop offset={0.82} stopColor="#A8396F" />
+        <stop offset={0.828} stopColor="#A7396E" />
+        <stop offset={0.836} stopColor="#A6396E" />
+        <stop offset={0.844} stopColor="#A5396E" />
+        <stop offset={0.852} stopColor="#A4396D" />
+        <stop offset={0.859} stopColor="#A3396D" />
+        <stop offset={0.867} stopColor="#A3396C" />
+        <stop offset={0.875} stopColor="#A2396C" />
+        <stop offset={0.883} stopColor="#A1396C" />
+        <stop offset={0.891} stopColor="#A0396B" />
+        <stop offset={0.898} stopColor="#9F396B" />
+        <stop offset={0.906} stopColor="#9E396A" />
+        <stop offset={0.914} stopColor="#9E396A" />
+        <stop offset={0.922} stopColor="#9D3869" />
+        <stop offset={0.93} stopColor="#9C3869" />
+        <stop offset={0.938} stopColor="#9B3869" />
+        <stop offset={0.945} stopColor="#9A3868" />
+        <stop offset={0.953} stopColor="#993868" />
+        <stop offset={0.961} stopColor="#993867" />
+        <stop offset={0.969} stopColor="#983867" />
+        <stop offset={0.977} stopColor="#973866" />
+        <stop offset={0.984} stopColor="#963866" />
+        <stop offset={0.992} stopColor="#953866" />
+        <stop offset={1} stopColor="#943865" />
+      </linearGradient>
+    </defs>
+    <g clipPath="url(#a)">
+      <g clipPath="url(#b)">
+        <g clipPath="url(#c)" transform="translate(283 236)">
+          <g clipPath="url(#d)">
+            <g clipPath="url(#e)">
+              <path fill="url(#f)" d="M.5.152V307.7h243V.152Zm0 0" />
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+    <g fill="#f6f6f6" clipPath="url(#g)" transform="translate(318 195)">
+      <path
+        fillOpacity={0.302}
+        d="M190.792 183.992c0 11.313-2.593 21.652-7.781 31.016-5.187 9.355-13.156 16.906-23.906 22.656-10.75 5.742-24.094 8.61-40.031 8.61h-29.47v70.046h-47.53V121.18h77c15.57 0 28.726 2.687 39.468 8.062 10.75 5.375 18.813 12.79 24.188 22.234 5.375 9.45 8.062 20.29 8.062 32.516Zm-75.328 24.469c9.082 0 15.848-2.13 20.297-6.39 4.445-4.259 6.672-10.286 6.672-18.079 0-7.781-2.227-13.8-6.672-18.062-4.45-4.258-11.215-6.391-20.297-6.391h-25.86v48.922Zm0 0"
+      />
+      <path
+        fillOpacity={0.502}
+        d="M178.507 171.706c0 11.313-2.594 21.653-7.782 31.016-5.187 9.355-13.156 16.906-23.906 22.656-10.75 5.742-24.094 8.61-40.031 8.61H77.319v70.046H29.788v-195.14h77c15.57 0 28.726 2.687 39.469 8.062 10.75 5.375 18.812 12.79 24.187 22.235 5.375 9.449 8.063 20.289 8.063 32.515Zm-75.329 24.47c9.082 0 15.848-2.13 20.297-6.392 4.446-4.257 6.672-10.285 6.672-18.078 0-7.78-2.226-13.8-6.672-18.062-4.449-4.258-11.215-6.39-20.297-6.39H77.32v48.921Zm0 0"
+      />
+      <path d="M166.22 159.42c0 11.313-2.593 21.653-7.78 31.016-5.188 9.356-13.157 16.906-23.907 22.656-10.75 5.743-24.093 8.61-40.031 8.61H65.033v70.047H17.502V96.609h77c15.57 0 28.727 2.687 39.469 8.062 10.75 5.375 18.812 12.789 24.187 22.234 5.375 9.45 8.063 20.289 8.063 32.516Zm-75.327 24.47c9.082 0 15.847-2.13 20.297-6.391 4.445-4.258 6.671-10.285 6.671-18.078 0-7.782-2.226-13.801-6.671-18.063-4.45-4.258-11.215-6.39-20.297-6.39h-25.86v48.921Zm0 0" />
+    </g>
+  </svg>
+);
